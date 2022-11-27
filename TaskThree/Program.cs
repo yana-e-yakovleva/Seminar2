2cec40140f23a3d1247b1072a2845ea8c6783554
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-28 01:05:14 +0300

    TaskThree

---
+++ TaskThree/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 6)
+{ Console.WriteLine("Выходной день");}
+if (day == 7)
+{ Console.WriteLine("Выходной день");}
+
+else
+{
+    Console.WriteLine("Будний день");
+}
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
