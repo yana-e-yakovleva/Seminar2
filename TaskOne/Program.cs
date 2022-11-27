0eb851a3ffaf482911e2ca922129748db1e40e48
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-28 00:38:24 +0300

    first commit

---
+++ TaskOne/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+
+int First = number / 100;
+int Second = (number - First*100)/10;
+
+Console.WriteLine($"Вторым числом является {Second}");
+
+
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
